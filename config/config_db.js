1e9ec4b412c8408995e3d214fc01e552e754a23e
imadtawab <89462211+imadtawab@users.noreply.github.com>
2024-11-08 12:18:27 +0100

    Update config_db.js

--- config/config_db.js
+++ config/config_db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 
 let connectToDB = (listen) => {
-    return mongoose.connect(process.env.DB).then(() => {
+    return mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
         console.log("DATABASE connected ...");
         listen()
     }).catch((err)=> rejectError(req , res , err))
