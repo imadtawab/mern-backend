9643c314c5e2289507581f7f25d3bb8f053f7d78
imadtawab <89462211+imadtawab@users.noreply.github.com>
2024-11-13 19:43:29 +0100

    Update config_db.js

--- config/config_db.js
+++ config/config_db.js
@@ -2,7 +2,10 @@ const mongoose=require('mongoose');
 
 
 let connectToDB = (listen) => {
-    return mongoose.connect(process.env.DB).then(() => {
+    return mongoose.connect(process.env.DB, {
+  serverSelectionTimeoutMS: 5000, // 5 seconds
+  socketTimeoutMS: 45000, // 45 seconds
+}).then(() => {
         console.log("DATABASE connected ...");
         listen()
     }).catch((err)=> rejectError(req , res , err))
