7c6dfa6063b716d28ba2aa6daa41ef743f9c60a1
imadtawab <89462211+imadtawab@users.noreply.github.com>
2024-11-18 12:56:45 +0100

    Update AccountControllers.js

--- admin/controllers/AccountControllers.js
+++ admin/controllers/AccountControllers.js
@@ -42,7 +42,7 @@ accountControllers.account_post_register = async (req, res, next) => {
         
         let storeOwner = newStore._id
         console.log("before hashPass")
-        let hashPass = await bcrypt.hash(password, +process.env.PASSWORD_KEY)
+        let hashPass = await bcrypt.hashSync(password, +process.env.PASSWORD_KEY)
         console.log("after hashPass : ", hashPass)
         let activationCode = await generateToken(req.body.email);
         console.log("after activationCode : " , activationCode)
